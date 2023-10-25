94151bb779ccfe55c757aae611a4116f43e6c708
keerthy31816 <148942818+keerthy31816@users.noreply.github.com>
2023-10-25 12:11:13 +0530

    routes.py

--- routes.py
+++ routes.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ProductListView, ProductDetailView
 
 urlpatterns = [
-    path('products/', ProductListView.as_view(), name='product-list'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
-    path('products/search/', ProductListView.as_view(), name='product-search'),
+    path('product/', ProductListView.as_view(), name='productlist'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='productdetail'),
+    path('product/search/', ProductListView.as_view(), name='productsearch'),
 ]
