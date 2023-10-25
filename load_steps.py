35a04de4ca721e2802acea32bb36537fcf106eb1
keerthy31816 <148942818+keerthy31816@users.noreply.github.com>
2023-10-25 12:09:59 +0530

    load_steps.py

--- load_steps.py
+++ load_steps.py
@@ -2,10 +2,10 @@ from behave import given
 from your_project.models import Product  # Import your Product model
 
 @given('I have the following products in the database')
-def step_create_products(context):
-    products_data = context.table  # Use a data table in your feature file
+def create_products(context):
+    products_data=context.table
     for row in products_data:
-        product = Product(
+        product=Product(
             name=row['name'],
             description=row['description'],
             price=row['price'],
